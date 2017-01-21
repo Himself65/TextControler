c2b5c8b0761ac0b48a2347e1c3296696caa8d1bc
扩散性百万甜面包 <761282619@qq.com>
2017-01-22 00:04:22 +0800

    操作界面还有问题

--- TextControler/AppControlMain.c
+++ TextControler/AppControlMain.c
@@ -9,9 +9,9 @@
 #include "AppControlMain.h"
 
 int AppControlMain(){
-    char pathname[256],txtname[256];
-    
     brd_puts("文件管理beta 0.1", stdout);
+    
+    
     
     brd_puts("[1]创建文件夹", stdout);
     brd_puts("[2]创建文件", stdout);
@@ -19,22 +19,28 @@ int AppControlMain(){
     for (; ; ) {
         brd_puts("输入操作", stdout);
         int operate = 0;
-        scanf("%u",&operate);
-        switch (operate) {
-            case 1:
-                brd_puts("enter the pathname", stdout);
-                brd_gets(pathname, 256, stdin);
-                brd_newDocument(pathname);
-                break;
-            case 2:
-                brd_puts("enter the txtname", stdout);
-                brd_gets(txtname, 256, stdin);
-                brd_newTxt(pathname, txtname);
-            case 0:
+        scanf("%d",&operate);
+        fflush(stdin);
+        char pathname[256],txtname[256];
+        for(;;){
+            if (operate) {
+                if (operate == 1) {
+                    brd_puts("enter the pathname", stdout);
+                    brd_gets(pathname, 256, stdin);
+                    brd_newDocument(pathname);
+                }
+                else if(operate == 2){
+                    brd_puts("enter the txtname", stdout);
+                    brd_gets(txtname, 256, stdin);
+                    brd_newTxt(pathname, txtname);
+                }
+                else if(operate == 0){
                 exit(EXIT_SUCCESS);
-            default:
-                brd_puts("输入错误", stdout);
-                break;
+                }
+                else{
+                    brd_puts("输入错误", stdout);
+                }
+            }
         }
     }
     return EXIT_SUCCESS;
