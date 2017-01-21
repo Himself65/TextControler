7178b456a27b3408111af5f03378054f37ce1398
扩散性百万甜面包 <761282619@qq.com>
2017-01-21 23:52:48 +0800

    用户界面

--- TextControler/AppControlMain.c
+++ TextControler/AppControlMain.c
@@ -9,17 +9,33 @@
 #include "AppControlMain.h"
 
 int AppControlMain(){
-    
     char pathname[256],txtname[256];
     
-    brd_puts("enter the pathname", stdout);
-    brd_gets(pathname, 256, stdin);
+    brd_puts("文件管理beta 0.1", stdout);
     
-    brd_puts("enter the txtname", stdout);
-    brd_gets(txtname, 256, stdin);
-    
-    brd_newDocument(pathname);
-    brd_newTxt(pathname, txtname);
-    
+    brd_puts("[1]创建文件夹", stdout);
+    brd_puts("[2]创建文件", stdout);
+    brd_puts("[0]退出程序", stdout);
+    for (; ; ) {
+        brd_puts("输入操作", stdout);
+        int operate = 0;
+        scanf("%u",&operate);
+        switch (operate) {
+            case 1:
+                brd_puts("enter the pathname", stdout);
+                brd_gets(pathname, 256, stdin);
+                brd_newDocument(pathname);
+                break;
+            case 2:
+                brd_puts("enter the txtname", stdout);
+                brd_gets(txtname, 256, stdin);
+                brd_newTxt(pathname, txtname);
+            case 0:
+                exit(EXIT_SUCCESS);
+            default:
+                brd_puts("输入错误", stdout);
+                break;
+        }
+    }
     return EXIT_SUCCESS;
 }
