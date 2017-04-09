22b906321529c4fdd8518896503d0b193fa464ea
扩散性百万甜面包 <761282619@qq.com>
2017-04-09 14:31:31 +0800

    不要ui

--- TextControler/TextControler.c
+++ TextControler/TextControler.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 #include "brd_io.h"
-#include"surfaceUI.h"
 
 
 int main(void) {
-	interfaceUI();
 	return EXIT_SUCCESS;
 }
 
--- TextControler/surfaceUI.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include"brd_io.h"
-
-void interfaceUI(void);
